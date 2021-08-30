62d3e87e45b26c7384d4d22c7e05ab4f2d545d4b
mil15 <emilpasko@gmail.com>
2021-08-30 17:03:50 +0200

    pierwsza

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+
+class Table extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      users:[],
+      isLoading:true,
+      isError: false
+    }
+  }
+//getrequest
+async componentDidMount(){
+  this.setState({isLoading:true})
+
+  const response=await fetch('https://api.artic.edu/api/v1/exhibitions')
+
+  if(response.ok){
+const users = await response.json()
+console.log(users)
+this.setState({users:users.data, isLoading:false})}
+else{
+  this.setState({isError:true, isLoading:false})
+}
+}
+
+renderTableHeader=()=> {
+return Object.keys(this.state.users[0]).map(attr =><th>
+{attr.toUpperCase()}
+  </th>
+)
+}
+renderTableRows=()=>{
+return this.state.users.map(user =>{
+  return(
+    <tr>
+
+
+    </tr>
+  )
+})
+}
+  render(){
+const{users, isLoading, isError}=this.state
+if(isLoading){
+  return <div>Loading...</div>
+  }
+if(isError){
+  return <div> error...</div>
+}
+
+  return users.length > 0
+  ? (
+<table>
+<thead>
+<tr>
+{this.renderTableHeader()}
+</tr>
+</thead>
+<tbody>
+{this.renderTableRows()}
+</tbody>
+</table>
+  ):(
+    <div>No users</div>
+  )
+
+}
+}
+
+export default Table;
